db51bd693b33d89c245648747f9922f97310de55
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2024-01-09 21:41:16 -0800

    Center items

--- src/app/components/atoms/title.tsx
+++ src/app/components/atoms/title.tsx
@@ -1,22 +1,22 @@
 interface TitleProps {
-    children: React.ReactNode,
-    classes?: string,
-    size?: string,
+  children: React.ReactNode,
+  classes?: string,
+  size?: string,
+}
+
+export default function Title({children, classes, size}: TitleProps) {
+  if (size === undefined){
+    size = "text-xl md:text-3xl"
   }
-  
-  export default function Title({children, classes, size}: TitleProps) {
-    if (size === undefined){
-      size = "text-xl md:text-3xl"
-    }
-    return (
-      <div className='flex flex-col justify-center items-center'>
-        <div className={`
-          text-center font-bold text-green-800 font-rufina
-          ${size} ${classes}
-        `}>
-          {children}
-        </div>
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <div className={`
+        font-bold text-green-800 font-rufina
+        ${size} ${classes}
+      `}>
+        {children}
       </div>
-    )
-  }
+    </div>
+  )
+}
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Title from './components/atoms/title'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-20 xl:p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 items-center justify-between font-mono text-sm lg:flex">
         <Title children="Proyectos que nos respaldan" />
       </div>
     </main>
