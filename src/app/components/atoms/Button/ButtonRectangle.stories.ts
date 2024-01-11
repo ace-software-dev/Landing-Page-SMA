d5b5387246a2b66a25d6f1cba9cbd5f551aeaba8
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-10 21:33:14 -0600

    refactor: restructure Storybook folder for atoms

--- src/app/components/atoms/Button/ButtonRectangle.stories.ts
+++ src/app/components/atoms/Button/ButtonRectangle.stories.ts
@@ -5,7 +5,7 @@ import RectangleButton from './button-rectangle';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/RectangleButton',
+  title: 'Atoms/RectangleButton',
   component: RectangleButton,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
