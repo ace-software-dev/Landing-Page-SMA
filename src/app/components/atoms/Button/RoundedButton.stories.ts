a41671cbc520ebc6a93b493b8ab0fc304a6cd6c4
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-10 23:22:43 -0600

    fix: rename Button component to RoundedButton

--- src/app/components/atoms/Button/RoundedButton.stories.ts
+++ src/app/components/atoms/Button/RoundedButton.stories.ts
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import Button from './button';
+import RoundedButton from './rounded-button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Atoms/Button',
-  component: Button,
+  title: 'Atoms/RoundedButton',
+  component: RoundedButton,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof RoundedButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
