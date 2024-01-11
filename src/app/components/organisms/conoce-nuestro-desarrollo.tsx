d28ca796fc4b165f10ba286694fc6f5d4ac27376
JR-espacial <jorge_alan_2001@hotmail.com>
2024-01-10 18:03:46 -0600

    add:fonts

---
+++ src/app/components/organisms/conoce-nuestro-desarrollo.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import RectangleButton from '../atoms/Button/button-rectangle';
+export default function Discover() {
+    
+    
+    return (
+    <section id="discover">
+            <RectangleButton>Glamping</RectangleButton>
+    </section>
+  )
+}
