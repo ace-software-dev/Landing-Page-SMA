a059c79688e37f06abc516c5e6f9ec8d42f47cb9
JR-espacial <jorge_alan_2001@hotmail.com>
2024-01-10 20:24:28 -0600

    add: add to page

--- src/app/components/atoms/Button/button-rectangle.tsx
+++ src/app/components/atoms/Button/button-rectangle.tsx
@@ -18,16 +18,12 @@ function isActive() {
  }
 }
 
-{/* <div className="w-36 h-12 px-4 py-3 bg-neutral-600 rounded-lg border border-neutral-600 justify-center items-center gap-2.5 inline-flex">
-  <div className="text-right text-gray-200 text-xl font-medium font-['Satoshi']">Glamping</div>
-</div> */}
-<div className="w-36 h-12 px-4 py-3 rounded-lg border-2 border-neutral-600 justify-center items-center gap-2.5 inline-flex">
-  <div className="text-right text-neutral-600 text-xl font-medium font-['Satoshi']">Cabaña</div>
-</div>
+//check fixed size
+//check font family
   return (
     <button type={type} className={`
         px-6 py-3 ${isActive()} rounded-lg border-2 border-green-primary font-satoshi font-medium text-xl
-        justify-center items-center inline-flex cursor-pointer hover:opacity-80 w-[9rem] ${classes} 
+        justify-center items-center inline-flex cursor-pointer hover:opacity-80 ${classes} 
         disabled:opacity-50 disabled:cursor-not-allowed ${disabled ? 'disabled' : ''}
       `}
       disabled={disabled}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Discover from "./components/organisms/conoce-nuestro-desarrollo"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>header</p>
-      </div>
+    <main className="flex min-h-screen flex-col items-center  p-24 bg-almost-white">
+        <div className="flex flex-row text-black"> 
+          <p>header</p>
+        </div>
+        <br/>
+        <Discover/>
     </main>
   )
 }
