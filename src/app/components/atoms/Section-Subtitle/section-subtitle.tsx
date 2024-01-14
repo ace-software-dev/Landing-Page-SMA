5746377976c968ccad686f099b9e942e661e8562
Felipe <feliyepez2001@gmail.com>
2024-01-13 20:57:53 -0500

    fix: responsive texts

--- src/app/components/atoms/Section-Subtitle/section-subtitle.tsx
+++ src/app/components/atoms/Section-Subtitle/section-subtitle.tsx
@@ -6,7 +6,7 @@ interface SubtitleProps {
   
   export default function SectionSubtitle({children, classes, size = 'text-base md:text-xl'}: SubtitleProps) {
     return (
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-full'>
         <div className={`text-subtitle ${size} text-center font-normal font-satoshi ${classes}`}>
           {children}
         </div>
