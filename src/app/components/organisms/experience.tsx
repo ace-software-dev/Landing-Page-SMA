d56594a8c952febb32d20e9b13486e70b308e8d9
Felipe <feliyepez2001@gmail.com>
2024-01-13 20:57:32 -0500

    feat: add experience swiper

---
+++ src/app/components/organisms/experience.tsx
@@ -0,0 +1,44 @@
+'use client'
+import SectionTitle from "../molecules/section-title"
+import Carousell from "../molecules/carousell"
+
+import Image from 'next/image';
+import { SwiperSlide } from 'swiper/react';
+
+import Arboleda from '../../../../public/images/Arboleda.png'
+import Espiga from '../../../../public/images/Espiga.png'
+import Latitud from '../../../../public/images/Latitud.png'
+import Molino from '../../../../public/images/Molino.png'
+
+
+export default function Experience() {
+  const images = [
+    { src: Molino, alt: "El Molino Residencial & Golf León"},
+    { src: Arboleda, alt: "La Arboleda Cabañas"},
+    { src: Latitud, alt: "Latitud La Victoria QRO"},
+    { src: Espiga, alt: "La Espiga Vida y Comunidad"},
+  ];
+
+  return (
+    <section id="experiencia" className='py-6 md:py-14 w-full'>
+      <div className='flex flex-col justify-center items-center mx-auto'>
+        <div className="w-full">
+          <SectionTitle title="Proyectos que nos respaldan" subtitle="20 años de experiencia colaborando en la gestión y dirección de proyectos inmobiliarios." />
+        </div>
+        <div className='flex justify-center items-center'>
+          <Carousell>
+            {images?.map((img, index) => (
+              <SwiperSlide key={index} className="!h-52">
+                <div className='flex h-full justify-center items-center'>
+                  <Image src={img.src} loading="lazy" alt={img.alt} className="w-auto h-auto max-w-40 max-h-40 md:max-w-52 md:max-h-52 object-cover filter grayscale" />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Carousell>
+        </div>
+      </div>
+
+
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Title from './components/atoms/Title/title'
 import Discover from './components/organisms/discover'
+import Experience from './components/organisms/experience'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-20 xl:p-24 bg-almost-white ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-12 md:p-16 lg:p-20 xl:p-24 bg-almost-white">
       <div className="flex">
         <Title>Proyectos que nos respaldan</Title>
       </div>
         <Discover/>
+        <Experience/>
     </main>
   )
 }
