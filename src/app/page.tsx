d4d108e3d794feedd48c1598d32a23b708e42634
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2024-01-15 11:58:22 -0800

    feat: job operations organism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Title from './components/atoms/Title/title'
 import Discover from './components/organisms/discover'
-
+import Operations from './components/organisms/operations'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-20 xl:p-24 bg-almost-white ">
-      <div className="flex">
-        <Title>Proyectos que nos respaldan</Title>
+    <main className="flex min-h-screen flex-col items-center bg-almost-white">
+      <div className="p-8 sm:p-12 md:p-16 lg:p-20 xl:p-24">
+          <Title>Proyectos que nos respaldan</Title>
+          <Discover/>
       </div>
-        <Discover/>
+      <Operations/>
     </main>
   )
 }
