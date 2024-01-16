e59c73d2264d8d2cc15a87fc3ecb17e41aa957d7
JR-espacial <jorge_alan_2001@hotmail.com>
2024-01-16 12:17:36 -0600

    fix: fix rectangle text

--- src/app/components/organisms/discover.tsx
+++ src/app/components/organisms/discover.tsx
@@ -7,10 +7,10 @@ export default function Discover() {
     
     return (
     <section id="discover" className=' m-6 gap-9 flex flex-col items-center max-w-xs sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl'>
-        <div className='flex flex-row w-[500px] gap-5 items-center'>
-            <RectangleButton classes='flex-1' active={false}>Casa Club</RectangleButton>
-            <RectangleButton classes='flex-1'>Glamping</RectangleButton>
-            <RectangleButton classes='flex-1' active={false}>Cabaña</RectangleButton>
+        <div className='flex flex-row w-full gap-5 items-center'>
+            <RectangleButton classes='flex-1 text-xs md:text-xl' active={false}>Casa Club</RectangleButton>
+            <RectangleButton classes='flex-1 text-xs md:text-xl'>Glamping</RectangleButton>
+            <RectangleButton classes='flex-1 text-xs md:text-xl' active={false}>Cabaña</RectangleButton>
         </div>
         <div>
             <ImageCard src ="/images/glamping_plano.png"></ImageCard>
