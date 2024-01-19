cddafd2b80be00995f2d13a0e4473f26987a1fc3
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-19 13:30:43 -0600

    fix: text and icon sizing for larger screens

--- src/app/components/molecules/ContactInformation/contact-information.tsx
+++ src/app/components/molecules/ContactInformation/contact-information.tsx
@@ -6,19 +6,19 @@ interface ContactInformationProps {
 
 export default function ContactInformation({}: ContactInformationProps) {
   return (
-    <div className="flex flex-1 flex-col p-6 items-start rounded-2xl bg-contact-info bg-center bg-no-repeat xs:bg-cover sm:bg-right-bottom gap-6 text-almost-white">
+    <div className="flex flex-1 flex-col p-6 md:p-8 items-start rounded-2xl bg-contact-info bg-center bg-no-repeat xs:bg-cover sm:bg-right-bottom gap-6 text-almost-white">
       <div className="flex flex-col items-start gap-2">
-        <div className="text-xl font-bold">Información de Contacto</div>
-        <div className="">¡Hablemos de tu próxima inversión!</div>
+        <div className="text-xl font-bold md:text-2xl">Información de Contacto</div>
+        <div className="md:text-xl">¡Hablemos de tu próxima inversión!</div>
       </div>
       <div className="flex flex-col justify-center gap-5">
         <div className="flex items-center gap-6">
-          <FontAwesomeIcon icon={faPhone} className='w-5 h-5 text-white text-lg' />
-          <div>+52 442 716 9906</div>
+          <FontAwesomeIcon icon={faPhone} className='w-5 h-5 md:w-6 md:h-6 text-white text-lg' />
+          <div className='md:text-xl'>+52 442 716 9906</div>
         </div>
         <div className="flex items-center gap-6">
-          <FontAwesomeIcon icon={faEnvelope} className='w-5 h-5 text-white text-lg' />
-          <div>miguel@saavedra.mx</div>
+          <FontAwesomeIcon icon={faEnvelope} className='w-5 h-5 md:w-6 md:h-6 text-white text-lg' />
+          <div className='md:text-xl'>miguel@saavedra.mx</div>
         </div>
       </div>
     </div>
