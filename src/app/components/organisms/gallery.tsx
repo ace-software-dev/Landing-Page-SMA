51372ce6b5742756359ed24c1a4b5c886ac1b963
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-21 20:24:53 -0600

    feat: make gallery responsive

--- src/app/components/organisms/gallery.tsx
+++ src/app/components/organisms/gallery.tsx
@@ -9,16 +9,16 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 const images = [
-  {src: '/images/gallery/1_cabin.jpg', alt: 'Cabaña'},
-  {src: '/images/gallery/2_bathroom.jpg', alt: 'Cabaña'},
-  {src: '/images/gallery/3_cabin.jpg', alt: 'Cabaña'},
-  {src: '/images/gallery/4_glamping.jpg', alt: 'Cabaña'},
+  {src: '/images/gallery/1_cabin.jpg', alt: 'Cabaña', description: 'Cabaña'},
+  {src: '/images/gallery/2_bathroom.jpg', alt: 'Cabaña', description: 'Cabaña'},
+  {src: '/images/gallery/3_cabin.jpg', alt: 'Cabaña', description: 'Cabaña'},
+  {src: '/images/gallery/4_glamping.jpg', alt: 'Cabaña', description: 'Glamping'},
 ];
 
 export default function Gallery() {
 
   return (
-    <section id="gallery" className='max-w-7xl w-full h-[32rem]'>
+    <section id="gallery" className='max-w-7xl w-full h-60 xs:h-72 sm:h-96 md:h-[32rem]'>
       <Swiper
         autoplay={{
           delay: 5000,
@@ -42,9 +42,14 @@ export default function Gallery() {
                 className='h-full w-full object-cover'
               />
               <div className="relative text-almost-white font-satoshi">
-                <div id='slide-description' className="flex flex-col bg-almost-black-40 w-full py-4 items-center justify-center gap-2 absolute bottom-0 left-0 text-3xl font-bold transition-opacity duration-500">
-                  Cabaña
-                  <div className="rounded-full border-2 border-almost-white px-5 py-3 hover:cursor-pointer text-xl">
+                <div id='slide-description' className="flex xs:flex-col bg-almost-black-40 w-full items-center justify-around
+                  gap-2 text-lg absolute bottom-0 left-0 font-bold transition-opacity duration-500 py-2
+                  sm:justify-center sm:text-xl sm:py-3 md:text-2xl lg:text-3xl lg:py-4
+                ">
+                  {image.description}
+                  <div className="rounded-full border-2 border-almost-white hover:cursor-pointer
+                    text-sm px-3 py-1.5 sm:text-lg sm:px-4 sm:py-2 lg:text-xl lg:px-5 lg:py-3
+                  ">
                     Contáctanos
                   </div>
                 </div>
