2adbf58f0746502f1415a0574e4cf0f1c436a49f
JR-espacial <jorge_alan_2001@hotmail.com>
2024-01-23 15:30:13 -0600

    feat: costum map js api

--- src/app/components/atoms/Map/map.tsx
+++ src/app/components/atoms/Map/map.tsx
@@ -1,12 +1,31 @@
 'use client'
 import React from 'react'
-import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, MarkerF, useJsApiLoader , InfoWindowF} from '@react-google-maps/api';
 
 
 const containerStyle = {
   width: '600px',
   height: '600px'
 };
+
+const places = [
+  {
+    lat: 20.913533166035844,
+    lng: -100.74378055060164,
+    name: 'Parroquia Centro'
+  },
+  {
+    lat: 20.898342336684696,
+    lng: -100.72659947263716,
+    name: 'Plaza la Luciérnaga'
+  },
+  {
+    lat: 20.93124265594304, 
+    lng: -100.6881409531946,
+    name: 'Splash Inn San Miguel'
+  },
+
+]
 
 const center = {
   lat: 20.927441183582008,
@@ -22,16 +41,14 @@ function MapAtom() {
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
     setMap(map)
   }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
   }, [])
+  
+   const [place, setPlace] = React.useState(null)
 
   return isLoaded ? (
       <GoogleMap
@@ -40,11 +57,76 @@ function MapAtom() {
         zoom={13}
         onLoad={onLoad}
         onUnmount={onUnmount}
+        
       >
         <MarkerF
-          title='Glamping Sanmiguel'
+          icon={'/images/redmarker.svg'}
+          title='Glamping San Miguel'
           position={center}
+          onClick={() => 
+            setPlace({ lat: 20.927441183582008, lng: -100.7104233679535 , name: 'Glamping San Miguel'})
+          }
         />
+        <InfoWindowF
+          position={center}
+          options={{
+            pixelOffset: {
+              width: 0,
+              height: -40,
+            },
+          }}
+  
+        >
+          <div className='rounded-lg'>
+            <p>Glamping San Miguel</p>
+          </div>
+        </InfoWindowF>
+
+        {places.map((place,i) => (
+          <div>
+          <MarkerF
+            icon={'/images/marker.svg'}
+            key={place.name + i.toString()}
+            title={place.name}
+            position={{ lat: place.lat, lng: place.lng }}
+            onClick={() => 
+              setPlace({ lat: place.lat, lng: place.lng , name: place.name})
+            }
+          />
+          <InfoWindowF 
+            position={{ lat: place.lat, lng: place.lng }} 
+            options={{
+              pixelOffset: {
+                width: 0,
+                height: -40,
+              },
+            }}
+          >
+            <div className='rounded-lg'>
+              <p>{ place.name}</p>
+            </div>
+          </InfoWindowF>
+          </div>
+        
+        ))}
+        
+        { place && <InfoWindowF
+          position={{ lat: place.lat, lng: place.lng }}
+          options={{
+            pixelOffset: {
+              width: 0,
+              height: -40,
+            },
+          }}
+          onCloseClick={() => setPlace(null)}
+          
+        >
+          <div className='rounded-lg'>
+            <p>{ place.name}</p>
+          </div>
+        </InfoWindowF>
+        }
+      
         { /* Child components, such as markers, info windows, etc. */ }
         <></>
       </GoogleMap>
--- src/app/components/organisms/location.tsx
+++ src/app/components/organisms/location.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import SectionTitle from '../molecules/SectionTitle/section-title';
 import MapAtom from '../atoms/Map/map';
+import RoundedButton from '../atoms/Button/rounded-button';
+
 
 
 export default function Location() {
   
   return (
-    <section id="contact" className='w-full bg-almost-white flex justify-center p-6 sm:p-10 md:p-14 font-satoshi'>
+    <section id="location" className='w-full  flex justify-center p-6 sm:p-10 md:p-14 font-satoshi'>
       <div className="w-full flex flex-col gap-4 sm:gap-6 md:gap-20 sm:flex-row max-w-[68rem]">
 
-        <div className='flex flex-row'>
+        <div className='flex flex-col gap-10 items-center justify-center'>
           <SectionTitle 
             title='Ubicación Privilegiada' 
             subtitle='Experimenta el lujo y la conveniencia de una gran ubicación. En tan solo 15 minutos, sumérgete en la magia del centro histórico y disfruta de todo lo que ofrece.'
           />
+          <div className='flex flex-row justify-center'>
+            <RoundedButton>
+              <a href="https://www.google.com/maps/place/20%C2%B055'38.8%22N+100%C2%B042'37.5%22W/@20.9274444,-100.7104167,17z" target="_blank">
+                Ver en Google Maps
+              </a>
+            </RoundedButton>
+          </div>
         </div>
 
-        <div className='flex flex-row w-full'>
-        {/* <GoogleMapsEmbed
-          apiKey="AIzaSyA2oBnN5Vzw8NDlZO7O6PlKrdY_OPXNLbY"
-          height={600}
-          width="600px"
-          mode="place"
-          q="20.927441183582008, -100.7104233679535"
-          language="es"
-          zoom='13'
-        /> */}
+        <div className='flex flex-col w-full'>
           <MapAtom/>
         </div>
 
