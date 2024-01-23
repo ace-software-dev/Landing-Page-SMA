488b142c60cfee91e8a450c398c32331f620c278
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-23 17:34:49 -0600

    feat: create installations section with responsive design

--- src/app/components/atoms/CircleNumber/circle-number.tsx
+++ src/app/components/atoms/CircleNumber/circle-number.tsx
@@ -5,8 +5,8 @@ interface CircleNumberProps {
 
 export default function CircleNumber({ children, classes }: CircleNumberProps) {
   return (
-    <div className={`w-16 h-16 md:w-24 md:h-24 bg-green-primary rounded-full justify-center items-center inline-flex ${classes}`}>
-      <div className="text-center text-white text-2xl sm:text-3xl md:text-4xl font-medium font-satoshi">
+    <div className={`w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 bg-green-primary rounded-full justify-center items-center inline-flex ${classes}`}>
+      <div className="text-center text-white text-2xl md:text-3xl lg:text-4xl font-medium font-satoshi">
         {children}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ import Timeline from './components/organisms/timeline'
 import Operations from './components/organisms/operations'
 import Contact from './components/organisms/contact'
 import Gallery from './components/organisms/gallery'
+import Installations from './components/organisms/installations'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center gap-12 md:gap-28 font-satoshi">
         <Header/>
         <Discover/>
+        <Installations />
         <Gallery />
         <Oportunity/>
         <Operations/>
