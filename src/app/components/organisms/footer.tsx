f0dfe9b95e3344cb9c298464c81ef5352d86a6c4
Andrea Piñeiro <andrea.pineiro.ca@gmail.com>
2024-01-24 21:28:04 -0800

    fix: add link to our web page and fix div styles

--- src/app/components/organisms/footer.tsx
+++ src/app/components/organisms/footer.tsx
@@ -3,14 +3,16 @@ import TextFooter from '../atoms/TextFooter/text-footer';
 
 export default function Discover() {
     return (
-        <div className="w-full bg-footer flex justify-center items-center py-2 lg:py-3">
+        <div className="w-full bg-footer flex justify-center items-center py-2 lg:py-3 px-6">
             <div className="space-y-2">
                 <TextFooter>*Las imágenes son referenciales.</TextFooter>
                 <TextFooter>*El proyecto está sujeto a modificaciones por parte del desarrollador.</TextFooter>
-                <div className='px-6 inline-flex'>
+                <div className='inline-flex'>
                     <TextFooter>© Glamping SMA.</TextFooter>
                     <TextFooter classes="w-7">|</TextFooter>
-                    <TextFooter>Desarrollado por Ace Software.</TextFooter>
+                    <a href="https://acesoftware.com.mx" className="underline text-almost-white">
+                        <TextFooter classes="hover:underline">Desarrollado por Ace Software.</TextFooter>
+                    </a>
                 </div>
             </div>
         </div>
