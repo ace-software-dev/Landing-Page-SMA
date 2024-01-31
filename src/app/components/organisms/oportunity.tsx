3293d98865c9cdc5f4011419cbc0ab45e8ab227f
pau-cardoso <paulinacardoso7@gmail.com>
2024-01-31 09:52:39 -0600

    fix: text corrections in opportunity cards

--- src/app/components/organisms/oportunity.tsx
+++ src/app/components/organisms/oportunity.tsx
@@ -7,11 +7,11 @@ import { SwiperSlide } from 'swiper/react';
 export default function Oportunity() {
   const images = [
     { src: '/images/Reservaciones.png', alt: "Reservaciones", text: "San Miguel de Allende dentro del Top #5 destinos de mayor crecimiento en 2023, con un aumento del 400% en reservas." },
-    { src: '/images/Romance.png', alt: "Romance", text: "Turismo romántico en San Miguel de Allende deja 3 mil millones en 2023, con un crecimiento proyectado del 10%." },
+    { src: '/images/Romance.png', alt: "Romance", text: "Turismo romántico en San Miguel de Allende deja 3 mil millones de pesos (MXN) en 2023, con un crecimiento proyectado del 10%." },
     { src: '/images/Oportunidad.png', alt: "Oportunidad de Negocio", text: "El concepto de Glamping aumenta 5 a 10 nuevas unidades de negocio al mes en México." },
     { src: '/images/Premiado.png', alt: "Destino Premiado", text: "Destino de lujo global, San Miguel de Allende, segunda Mejor Ciudad del Mundo según la WBA." },
-    { src: '/images/Retorno.png', alt: "Retorno prometedor", text: "Ofrecemos ROI de 24% el primer año, 36% a partir del segundo año y recuperación total en el tercer año." },
-    { src: '/images/Experiencia.png', alt: "Buena Experiencia", text: "Gestión de Casa Club con atención en cada detalle, dando experiencias excepcionales para nuestros inversionistas." },
+    { src: '/images/Retorno.png', alt: "Retorno prometedor", text: "Ofrecemos ROI de 20% el primer año, 30% a partir del segundo año y recuperación total en el tercer año." },
+    { src: '/images/Experiencia.png', alt: "Buena Experiencia", text: "Casa Club con atención en cada detalle, dando experiencias excepcionales para nuestros inversionistas." },
   ];
 
   return (
